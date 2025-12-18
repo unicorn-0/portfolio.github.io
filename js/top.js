1f03546eeac67efbf3a4ea04cf8b091a87e62fc3
sakamoto <t0mgymmfxt9pu3dqb1tb@gmail.com>
2025-12-18 22:57:01 +0900

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,43 +1,40 @@
-$(function(){
-    $(window).on("scroll",function(){
-        if ($(window).scrollTop() > 30){
-            $("header").addClass("active_header");
-            $(".first_work").addClass("active_first_work");
-        } else {
-            $("header").removeClass("active_header");
-            $(".first_work").removeClass("active_first_work");
-        }
-    });
+$(function() {
+    $(window).on('scroll', function() {
+        let scrollTop = $(window).scrollTop();
 
-    $(window).on("scroll",function(){
-        if($(window).scrollTop() > 60){
-            $(".first_work").addClass("active_first_work");
+        // 1. ヘッダーを消す (30px以上)
+        if (scrollTop > 30) {
+            $('header').addClass('active_header');
         } else {
-            $(".first_work").removeClass("active_first_work");
+            $('header').removeClass('active_header');
         }
-    });
 
-    $(window).on("scroll",function(){
-        if($(window).scrollTop() > 90){
-            $(".second_work").addClass("active_second_work");
+        // 2. 1番目の作品を出す (60px以上)
+        if (scrollTop > 60) {
+            $('.first_work').addClass('active_first_work');
         } else {
-            $(".second_work").removeClass("active_second_work");
+            $('.first_work').removeClass('active_first_work');
         }
-    });
 
-    $(window).on("scroll",function(){
-        if($(window).scrollTop() > 120){
-            $(".third_work").addClass("active_third_work");
+        // 3. 2番目の作品を出す (90px以上)
+        if (scrollTop > 90) {
+            $('.second_work').addClass('active_second_work');
         } else {
-            $(".third_work").removeClass("active_third_work");
+            $('.second_work').removeClass('active_second_work');
         }
-    });
 
-    $(window).on("scroll",function(){
-        if($(window).scrollTop() > 150){
-            $(".other_work").addClass("active_other_work");
+        // 4. 3番目の作品を出す (120px以上)
+        if (scrollTop > 120) {
+            $('.third_work').addClass('active_third_work');
         } else {
-            $(".other_work").removeClass("active_other_work");
+            $('.third_work').removeClass('active_third_work');
+        }
+
+        // 5. その他の作品を出す (150px以上)
+        if (scrollTop > 150) {
+            $('.other_work').addClass('active_other_work');
+        } else {
+            $('.other_work').removeClass('active_other_work');
         }
     });
 });
